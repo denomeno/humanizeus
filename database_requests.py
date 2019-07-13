4c408b84e4dce6ec578581cb5e1ad8d3556183c7
Boray Toktay <49341065+boraytoktay@users.noreply.github.com>
2019-07-13 09:42:08 -0400

    Update database_requests.py

--- database_requests.py
+++ database_requests.py
@@ -3,20 +3,19 @@
 import time
 import mysql.connector
 
+"""
 cnx = mysql.connector.connect(user='root', password='',
                                 host='localhost',
                                 database='homeless_project')
+"""
 
-r"""
 host = "humanize-us.c1xrcwz9rqrf.us-east-2.rds.amazonaws.com"
-
 port = "3306"
 
-
 cnx = mysql.connector.connect(user='humanizeusmaster', password='humanproject',
-                                    host=host, port = port)
-                                    #database='humanize-us')
-"""#to be implemented once database is migrated
+                                    host=host, port = port,
+                                    database='humanizeusmaster')
+
 
 class Database_requests:
 
