e59f4173a5b890c0bbc36bda4fd26d31072d5714
denomeno <hallikdeniz@gmail.com>
2019-07-14 14:49:50 -0400

    Update index.py

--- index.py
+++ index.py
@@ -134,20 +134,20 @@ li a:hover {
 def print_bottom_of_page():
      '''Print the bottom of the HTML page.'''
 
-    print('''
-    <br>
-    <br>
-    <br>
-    <hr>
-    <div style="background-color:#e5e5e5;text-align:center;padding:10px;margin-top:7px;">
-    <u>©humanizeus</u>
-    This page was generated at %s.<br>
-    Return to the <a href="/humanizeus/index.py">main page</a>.<br>
-    Enter <a href="/humanizeus/admin.py">admin portal</a>.</p>
-    </footer>
-    </body>
-    </html>
-    ''' % time.ctime())
+     print('''
+     <br>
+     <br>
+     <br>
+     <hr>
+     <div style="background-color:#e5e5e5;text-align:center;padding:10px;margin-top:7px;">
+     <u>©humanizeus</u>
+     This page was generated at %s.<br>
+     Return to the <a href="/humanizeus/index.py">main page</a>.<br>
+     Enter <a href="/humanizeus/admin.py">admin portal</a>.</p>
+     </footer>
+     </body>
+     </html>
+     ''' % time.ctime())
 
 
  #####################################################################################
