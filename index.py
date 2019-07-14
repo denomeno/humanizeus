071e8db18c1890c1e0db7584119a43eda661d29c
Boray Toktay <49341065+boraytoktay@users.noreply.github.com>
2019-07-14 15:16:25 -0400

    Update index.py

--- index.py
+++ index.py
@@ -39,36 +39,36 @@ def print_top_of_page():
       '''Print the top of the HTML page.'''
       '''<meta name="viewport" content="width=device-width, initial-scale=1.0">'''
 
-      print("""
-      <html>
-      <head>
-      <meta charset='utf-8'>
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <style>
+    print("""
+    <html>
+    <head>
+    <meta charset='utf-8'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
 
-      h1 {
-      color: red
-      font-family: "Garamound";
-      }
+    h1 {
+    color: red
+    font-family: "Garamound";
+    }
 
-      body {
-      background-color: white;
-      }
+    body {
+    background-color: white;
+    }
 
-      footer {
-      color: black;
-      text-align: left;
-      padding: 2px 10px 0px 0px;
-      clear: both
-      }
+    footer {
+    color: black;
+    text-align: left;
+    padding: 2px 10px 0px 0px;
+    clear: both
+    }
 
-      </style>
-      </head>
-      <body>
-      """)
+    </style>
+    </head>
+    <body>
+    """)
 
-      #text-align: center;
+    #text-align: center;
 
 
 #####################################################################################
