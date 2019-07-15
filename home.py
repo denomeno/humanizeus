b855e24eaee34f4d79b3da36e6f6e087f2d2ada9
denomeno <hallikdeniz@gmail.com>
2019-07-15 16:23:21 -0400

    Update home.py

--- home.py
+++ home.py
@@ -9,9 +9,9 @@ def home_page_display():
 
     print('''
     <h2>Building a community starts here!</h2>
-    <button type="button" style="height:200px;width:200px" align="left"><a href='/humanizeus/add_need.py'>I need support</a></button>
-    <button type="button" style="height:200px;width:200px" align="center"><a href='/humanizeus/add_support.py'>I want to donate</a></button>
-    <button type="button" style="height:200px;width:200px" align="right"><a href='/humanizeus/for_organizations.py'>I am an organization</a></button>
+    <button type="button" style="font-size: 20px height:200px;width:200px" align="left"><a href='/humanizeus/add_need.py'>I need support</a></button>
+    <button type="button" style="font-size: 20px height:200px;width:200px" align="center"><a href='/humanizeus/add_support.py'>I want to donate</a></button>
+    <button type="button" style="font-size: 20px height:200px;width:200px" align="right"><a href='/humanizeus/for_organizations.py'>I am an organization</a></button>
 
     ''')
 
