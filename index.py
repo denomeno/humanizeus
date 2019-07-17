58baeed8cddd10efd02351da30523697ed03e584
denomeno <hallikdeniz@gmail.com>
2019-07-16 20:57:23 -0400

    Update index.py

--- index.py
+++ index.py
@@ -129,7 +129,7 @@ li a:hover {
 def print_bottom_of_page():
     '''Print the bottom of the HTML page.'''
 
-    now_time = datetime.now(timezone('US/Eastern'))
+    #now_time = datetime.now(timezone('US/Eastern'))
 
     print('''
     <br>
@@ -144,7 +144,8 @@ def print_bottom_of_page():
     </div>
     </body>
     </html>
-    ''' % now_time.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
+    ''' % datetime)
+    #now_time.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
     #% time.strftime("%a, %d %b %Y %H:%M:%S"))
 
 
