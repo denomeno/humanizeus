297cdc3e2a033bbc7893f17e2d65983e3ecff27c
denomeno <hallikdeniz@gmail.com>
2019-07-18 08:52:03 -0400

    Update mission.py

--- mission.py
+++ mission.py
@@ -7,11 +7,13 @@ from import_modules import *
 
 def description_of_mission():
     print('''
-    <h3>We have a mission.</h3>
-    <u>Mission:</u> HumanizeUs’s mission is to provide a technical solution to improve access to essential home items for persons that are entering stable housing in our communities.
+    <h2>We have a mission.</h2>
+    <u>Mission:</u> HumanizeUs's mission is to provide a technical solution to improve access to essential home items for persons that are entering stable housing in our communities.
 
+    <br>
     <u>Vision:</u> We envision a world where all of our neighbors support each other to have a healthy and sustainable home.
 
+    <br>
     HumanizeUs is a non-profit dedicated to fighting homelessness by connecting the community through enabling people to donate directly to those in their local community that are in need. We mobilize resources via geographical information systems. Our online platform changes the traditional model of donating to organizations by centralizing the needs of organizations working in the field and the resources of the community to systematically deliver newly housed people with essential living items.
 
     <h5>Thank you for joining our cause!<h5>
