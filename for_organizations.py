5d460e8585a5b71c4b0b14c03acd87b7906ceb2b
denomeno <hallikdeniz@gmail.com>
2019-07-29 15:43:50 -0400

    login

--- for_organizations.py
+++ for_organizations.py
@@ -11,9 +11,21 @@ def view_existing_organizations_login():
     print('''
     <h3>Existing member?</h3><br>
     <form method=POST >
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
         <input type="hidden" name="form_name" value="existingMemberLogin"/>
-        <input name="login_email" type="email">
-        <input type='submit' value='Login'>
+        <input name="login_email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      </div>
+      <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Login</button>
     </form>
     ''')
 
